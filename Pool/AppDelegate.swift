da748a0bb683f3ddeca64e52f459f9bd20cdce34
Maculate Conception <aural.student@gmail.com>
2017-08-04 23:41:54 -0700

    Initial Commit

---
+++ Pool/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Pool
+//
+//  Created by Kay Ven on 8/4/17.
+//  Copyright (c) 2017 anon. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet weak var window: NSWindow!
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+}
